68d090332124a2af1d60e034a77e6b351a02c1d9
Fabio Cruz <fabio.cruz@etus.group>
2021-02-01 16:37:08 -0300

    :construction: Create app header

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,47 @@
 <template>
   <v-app>
-    <v-app-bar app color="primary" dark>
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <v-app-bar clipped-left app color="#EB5E28" dark>
+      <v-app-bar-nav-icon @click.stop="mini = !mini"></v-app-bar-nav-icon>
+      <v-spacer />
+      <v-toolbar-title>
+        <v-img src="./assets/logo.png" width="100"></v-img>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-toolbar-icon>
+        <v-icon>mdi-account</v-icon>
+      </v-toolbar-icon>
     </v-app-bar>
+
+    <v-navigation-drawer clipped app dark v-model="drawer" :mini-variant.sync="mini" permanent>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="title">
+            AGENDA ON
+          </v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list
+        dense
+        nav
+      >
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
 
     <v-main>
       <HelloWorld />
@@ -50,7 +60,13 @@ export default {
   },
 
   data: () => ({
-    //
+    drawer: false,
+    items: [
+      { title: 'HOME', icon: 'mdi-view-dashboard' },
+      { title: 'AGENDAMENTOS', icon: 'mdi-forum' },
+    ],
+    links: ['Home', 'Contacts', 'Settings'],
+    mini: true
   })
 };
 </script>
